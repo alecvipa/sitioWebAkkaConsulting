96d900fac470ab09b591794505d5795d71f26d63
alecvipa <hebrit_626@hotmail.com>
2021-09-26 18:52:27 -0500

    adds

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.post("/ajax/email", function (request, response) {
      <p>Message: ${request.body.message}</p>`;
     var mail = {
         from: '"Team: Xyncs Web Studio',
-        to: 'alecvipa@gmail.com',
+        to: 'hebrit_626@hotmail.com',
         subject: '¡Alguien ha dejado sus datos en tu sitio web!',
         html: htmlBody
     };
